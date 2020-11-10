d68c64ca0ba66ba5854f2973303fa73892faf344
John <john1997@live.ca>
2020-11-10 17:42:03 -0500

    Folder Page updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Course from './Course.js';
 import Folder from './Folder.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft  } from '@fortawesome/free-solid-svg-icons';
 import { Navbar, Nav, Row, Col } from 'react-bootstrap';
 
 class App extends React.Component {
@@ -16,24 +18,25 @@ class App extends React.Component {
     return (
       <Router>
         <Navbar variant="temp" className="headbg" >
+          <Nav>
           <Row>
             <Col>
               <Navbar.Brand as={Link} to="/" >CourseAlly</Navbar.Brand>
             </Col>
-            <Nav className="mr-auto">
+            
               <Col>
                 <Nav.Link as={Link} to="/" >Home</Nav.Link>
                 <Nav.Link as={Link} to="/Course" >Course</Nav.Link>
                 <Nav.Link as={Link} to="/Folder" >Folder</Nav.Link>
               </Col>
-            </Nav>
-            <Nav>
+          
               <Col>
                 <Nav.Link as={Link} to="/Folder" >{this.state.heading}</Nav.Link>
-                <Nav.Link as={Link} to="/">&lt; Back</Nav.Link>
+                <Nav.Link as={Link} to="/"><FontAwesomeIcon icon={faAngleLeft} size="lg" /> Back</Nav.Link>
               </Col>
-            </Nav>
+            
           </Row>
+          </Nav>
         </Navbar>
 
         <Route exact path="/" component={Home} />
--- src/Folder.js
+++ src/Folder.js
@@ -2,43 +2,122 @@ import React, { Component } from 'react';
 import './Folder.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons';
-import { Col, Row, Container } from 'react-bootstrap';
+import { faCloudUploadAlt, faSortAmountDown, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row, Container, ListGroup, Tab } from 'react-bootstrap';
+import test1 from './pdf/TEST1.pdf';
+import test2 from './pdf/TEST2.pdf';
+import test3 from './pdf/TEST3.pdf';
+import test4 from './pdf/TEST4.pdf';
+import test5 from './pdf/TEST5.pdf';
+import test6 from './pdf/TEST6.pdf';
+import readmepdf from './pdf/README.pdf';
 
 
 class Folder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { doc: test1 }
+    }
 
     render() {
         return (
             <div class="folder">
                 <Container fluid={true} >
-                    <Row>
-                        <Col>
-                            <div class="vline">
-                                <Row>
-                                    <Col></Col>
-                                    <Col>
-                                    <div class="files"><h5>Files</h5></div>
-                                    </Col>
-                                    <Col></Col>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                    Filter
-                                    </Col>
-                                    <Col></Col>
-                                    <Col>
-                                    <div class="upload">
-                                    <FontAwesomeIcon icon={faCloudUploadAlt} size="2x"/>
-                                    </div>
-                                    </Col>
-                                </Row>
-                            </div>
-                        </Col>
-                        <Col >
-                            te
-                    </Col>
-                    </Row>
+                    <Tab.Container defaultActiveKey="#link1">
+                        <Row>
+                            <Col>
+                                <div class="vline">
+                                    <Row>
+                                        <Col></Col>
+                                        <Col>
+                                            <div class="files"><h4>Files</h4></div>
+                                        </Col>
+                                        <Col></Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <div class="sort"><b class="size">Sort By </b><FontAwesomeIcon icon={faSortAmountDown} size="lg" /></div>
+                                        </Col>
+                                        <Col></Col>
+                                        <Col>
+                                            <div class="upload">
+                                                <FontAwesomeIcon icon={faCloudUploadAlt} size="2x" />
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs="1"></Col>
+                                        <Col>
+                                            <div class="table">
+
+                                                <ListGroup as="ul">
+                                                    <ListGroup.Item as="li" variant="secondary" ><b>File Name</b></ListGroup.Item>
+                                                    <ListGroup.Item as="li" action variant="secondary" href="#link1" >thermals 1.docx</ListGroup.Item>
+                                                    <ListGroup.Item as="li" action variant="secondary" href="#link2" >thermals 3.docx</ListGroup.Item>
+                                                    <ListGroup.Item as="li" action variant="secondary" href="#link3" >fluids ex 1.jpeg</ListGroup.Item>
+                                                    <ListGroup.Item as="li" action variant="secondary" href="#link4" >thermals 4.docx</ListGroup.Item>
+                                                    <ListGroup.Item as="li" action variant="secondary" href="#link5" >fluids 5.docx</ListGroup.Item>
+                                                    <ListGroup.Item as="li" action variant="secondary" href="#link6" >thermals 6.pdf</ListGroup.Item>
+                                                    <ListGroup.Item as="li" action variant="secondary" href="#link7" >thermals - jjs notes.pdf</ListGroup.Item>
+                                                </ListGroup>
+
+
+                                            </div>
+                                        </Col>
+                                        <Col xs="1"></Col>
+                                    </Row>
+                                </div>
+                            </Col>
+                            <Col >
+                                <div class="pdf">
+                                    <Tab.Content>
+                                        <Tab.Pane eventKey="#link1">
+                                            <div class="docHead">Document Preview &nbsp;
+                                                <a href={test1} target='_blank'><FontAwesomeIcon icon={faExpandAlt} size="lg" /></a>
+                                            </div>
+                                            <embed src={test1} width="700px" height="700px" />
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="#link2">
+                                        <div class="docHead">Document Preview &nbsp;
+                                                <a href={test2} target='_blank'><FontAwesomeIcon icon={faExpandAlt} size="lg" /></a>
+                                            </div>
+                                            <embed src={test2} width="700px" height="700px" />
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="#link3">
+                                        <div class="docHead">Document Preview &nbsp;
+                                                <a href={test3} target='_blank'><FontAwesomeIcon icon={faExpandAlt} size="lg" /></a>
+                                            </div>
+                                            <embed src={test3} width="700px" height="700px" />
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="#link4">
+                                        <div class="docHead">Document Preview &nbsp;
+                                                <a href={test4} target='_blank'><FontAwesomeIcon icon={faExpandAlt} size="lg" /></a>
+                                            </div>
+                                            <embed src={test4} width="700px" height="700px" />
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="#link5">
+                                        <div class="docHead">Document Preview &nbsp;
+                                                <a href={test5} target='_blank'><FontAwesomeIcon icon={faExpandAlt} size="lg" /></a>
+                                            </div>
+                                            <embed src={test5} width="700px" height="700px" />
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="#link6">
+                                        <div class="docHead">Document Preview &nbsp;
+                                                <a href={readmepdf} target='_blank'><FontAwesomeIcon icon={faExpandAlt} size="lg" /></a>
+                                            </div>
+                                            <embed src={readmepdf} width="700px" height="700px" />
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="#link7">
+                                        <div class="docHead">Document Preview &nbsp;
+                                                <a href={test6} target='_blank'><FontAwesomeIcon icon={faExpandAlt} size="lg" /></a>
+                                            </div>
+                                            <embed src={test6} width="700px" height="700px" />
+                                        </Tab.Pane>
+                                    </Tab.Content>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Tab.Container>
                 </Container>
             </div>
         );
